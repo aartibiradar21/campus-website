8f2ef759f8dbb6d0a0da9bd15fd16929cf51d08d
Rani Rathod <ranirathor20@navgurukul.org>
2024-10-05 19:00:26 +0530

    secont page

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 
 // Page.js
 import React from 'react';
-import Header from './Header.js'; 
+// import Header from './Header.js'; 
 
 const Page = () => {
   return (
     <div>
-      <Header />
+      {/* <Header /> */}
     </div>
   );
 };
