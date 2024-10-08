f3cb6e8cceee81595003d925c7e306df4760e768
Aarti Biradar <aartibiradar21@navgurukul.org>
2024-10-08 13:45:54 +0530

    solve error

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
 import React from 'react';
 // import Header from './Header.js'; 
+import Content from './Content';
 
 const Page = () => {
   return (
     <div>
       {/* <Header /> */}
+
+      <Content/>
     </div>
   );
 };
